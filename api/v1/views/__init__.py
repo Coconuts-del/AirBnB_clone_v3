69b26c1fa253d8482abf953ff50e03168e6a61ec
Coconuts-del <6712@holbertonstudents.com>
2023-12-21 16:29:08 +0100

    avoid circular import

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,6 +2,9 @@
 """ Init file """
 
 from flask import Blueprint
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
 from api.v1.views.amenities import *
 from api.v1.views.index import *
 from api.v1.views.states import *
@@ -9,5 +12,3 @@ from api.v1.views.cities import *
 from api.v1.views.places_reviews import *
 from api.v1.views.users import *
 from api.v1.views.places import *
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
