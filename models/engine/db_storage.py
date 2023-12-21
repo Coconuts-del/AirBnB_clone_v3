ed880dd03ae1a067a0ac44d48d04f877d355f328
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-12-21 12:59:09 +0100

    adding count function

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,9 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """class and id"""
+        key = cls.__name__ + "." + id
+        return self.__session.query(cls).filter_by(id=id).first()
+
